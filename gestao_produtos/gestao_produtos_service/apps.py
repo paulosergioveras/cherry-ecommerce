3ac723cf74ef6ad10fad14f9ed419537eb094f3c
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-09-27 17:46:35 -0300

    initial commit

---
+++ gestao_produtos/gestao_produtos_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestaoProdutosServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestao_produtos_service'
---
+++ gestao_usuarios/gestao_usuarios_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GestaoUsuariosServiceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gestao_usuarios_service'
