7ee384a538b144004436a59552e11ddf8adfaae8
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-10-26 14:44:39 -0300

    feat: atualiza secret_key para ser a mesma tem todos os microservices

--- gateway/app/settings.py
+++ gateway/app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5gx72=0l=-i)j9r7u33oy+tcq-ssc(f01(e-@^c%23l(1bcfxt'
+SECRET_KEY = 'django-insecure-7$3=d#z(anyos37xujs%tfc1z_)$1^w*i1t_%sn40(lss(k28+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
