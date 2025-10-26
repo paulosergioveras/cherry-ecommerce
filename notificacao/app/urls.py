746eb74617e82abc18d60255b6bddab82d58cc90
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-10-25 22:00:51 -0300

    feat: add documentacao

--- notificacao/app/urls.py
+++ notificacao/app/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cherry Ecommerce Notificação Microservice",
+        default_version='v1',
+        description="API for Cherry Ecommerce",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="paulosergiovc12@gmail.com"),
+        license=openapi.License(name="MIT License"),    
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/notificacao/', include('notificacao_service'))
+    path('api/v1/notificacao/', include('notificacao_service')),
+    
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
