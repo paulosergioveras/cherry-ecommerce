e77bb3906763148d78610dfd69913e6050b5bd5a
Paulo <85592000+paulosergioveras@users.noreply.github.com>
2025-10-25 22:05:28 -0300

    feat: add documentacao

--- pagamento/app/settings.py
+++ pagamento/app/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
-    #'corsheaders',
+    'drf_yasg',
 
     'pagamento_service',
 ]
